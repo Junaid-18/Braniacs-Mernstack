8ec9087afe33617165ba3cc97ed8ce2dfc0c949e
Muhammad Umair <147268250+Muhammad-Umair-developer@users.noreply.github.com>
2025-06-01 00:13:58 +0500

    Updated

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -3,7 +3,7 @@ import { assets } from '../../assets/assets'
 
 const CallToAction = () => {
   return (
-    <div className='flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0'>
+    <div className='flex flex-col items-center gap-4 pb-24 px-8 md:px-0'>
       <h1 className='md:text-4xl text-xl text-gray-800 font-semibold'>Learn anything, anytime, anywhere</h1>
       <p className='text-gray-500 sm:text-sm'>Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
       <div className='flex items-center font-medium gap-6 mt-4'>
